2caaf46385a91c6312de3a629b5615cade494e9c
Ajay Salaskar <Ajay Salaskar@AAIAJAYPC>
2018-04-28 18:17:25 +0530

    new commit

--- EnteringAgain/src/enterToDo/trialPost.java
+++ EnteringAgain/src/enterToDo/trialPost.java
@@ -4,8 +4,21 @@ public class trialPost {
 
 	public static void main(String args[]){
 		System.out.println("Testing adding addtional files to the repository");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
 	}
 	
-	
+
+	public void display(){
+		System.out.println("displaying trialPost");
+	}
 	
 }
+
+
+// can add a normal java file which can have the main(), to the servlet and call the all the functions of the file inside 
+//the servlet. The output shows up in the console . The servlet is displayed in the main window.
+//To call the main(), from the servlet, a string array has to be created and passed to the call like eg 
+//String[] array = {"this", "that"};
+//tp.main(array);
+//output again is found in the console
